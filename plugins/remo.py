5ecc62285381349506a3f7541d3b21b89114cbad
Toshiaki Hatano <haeena@haeena.net>
2018-08-11 23:33:58 +0900

    WIP select room

--- plugins/remo.py
+++ plugins/remo.py
@@ -13,11 +13,35 @@ nature_config.access_token = API_TOKEN = os.getenv("NATURE_ACCESS_TOKEN")
 nature_api_client = NatureApiClient(configuration=nature_config)
 nature_api = NatureApi(nature_api_client)
 
-def room_info(event_data):
-    message = event_data["event"]
-    if message.get("subtype") is None and "room" in message.get('text'):
-        channel = message["channel"]
+def post_select_room_action(slack_client, channel, user=None, ephemeral=False):
+    nature_devices = nature_api.v1_devices_get()
 
+    options = [{"text": dev["name"], "value": dev["name"]} for dev in nature_devices]
+
+    attachments = [
+        {
+            "fallback": "Upgrade your Slack client to use messages like these.",
+            "color": "#258ab5",
+            "attachment_type": "default",
+            "callback_id": "select_room",
+            "actions": [
+                {
+                    "name": "device_list",
+                    "text": "Select room",
+                    "type": "select",
+                    "options": options
+                }
+            ]
+        }
+    ]
+
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        attachments=attachments
+    )
+
+def room_info(slack_client, channel, user=None, ephemeral=False):
     nature_device_status = nature_api.v1_devices_get()
 
     status_lines = []
@@ -34,4 +58,8 @@ def room_info(event_data):
             lines += " {}Light".format(events["il"]["val"])
         status_lines.append(lines)
 
-    message.send("\n".join(status_lines))
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text="\n".join(status_lines)
+    )
--- run.py
+++ run.py
@@ -1,21 +1,63 @@
+from flask import Flask
+from flask import request, make_response, Response
 from slackeventsapi import SlackEventAdapter
 from slackclient import SlackClient
 import os
+import json
+from pprint import pprint
+
+from plugins.remo import room_info, post_select_room_action
+
+app = Flask(__name__)
 
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
-slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/omoikane/events")
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/omoikane/events", app)
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 slack_client = SlackClient(SLACK_BOT_TOKEN)
 
+@app.route("/omoikane/interactive", methods=["POST"])
+def handle_interactive_post():
+    request_body = json.loads(request.form["payload"])
+    pprint(request_body)
+
+    if request_body["token"] != SLACK_SIGNING_SECRET:
+        return make_response("", 400)
+
+    channel_id = request_body["channel"]["id"]
+    user_id = request_body["user"]["id"]
+
+    val = request_body["actions"][0]["value"]
+    if val == "kinoko":
+        response_text = "よろしい、ならば戦争だ"
+    else:
+        response_text = "よろしい、ならば盟友だ"
+
+    response = slack_client.api_call(
+        "chat.postMessage",
+        channel=channel_id,
+        text=response_text,
+        attachments=[]
+    )
+
+    return make_response("", 200)
+
 @slack_events_adapter.on("message")
 def handle_message(event_data):
+    pprint(event_data)
+
     message = event_data["event"]
     # If the incoming message contains "hi", then respond with a "Hello" message
     if message.get("subtype") is None and "hi" in message.get('text'):
         channel = message["channel"]
-        message = "Hello <@%s>! :tada:" % message["user"]
-        slack_client.api_call("chat.postMessage", channel=channel, text=message)
+        response_text = "Hello <@%s>! :tada:" % message["user"]
+        slack_client.api_call("chat.postMessage", channel=channel, text=response_text)
+    if message.get("subtype") is None and "room" in message.get('text'):
+        channel = message["channel"]
+        room_info(slack_client, channel)
+    if message.get("subtype") is None and "ctrl" in message.get('text'):
+        channel = message["channel"]
+        post_select_room_action(slack_client, channel)
 
 # Example reaction emoji echo
 @slack_events_adapter.on("reaction_added")
@@ -28,5 +70,5 @@ def reaction_added(event_data):
 
 if __name__ == "__main__":
     # Once we have our event listeners configured, we can start the
-    # Flask server with the default `/events` endpoint on port 3000
-    slack_events_adapter.start(host='0.0.0.0',port=3000)
+    # Flask server with the default `/events` endpoint on port 300
+    app.run(host='0.0.0.0',port=3000)
